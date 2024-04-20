c1b898bf47876236e8af07eda03c228943b576fc
iamharikrishnaa <iamharikrishnaa@gmail.com>
2024-04-20 13:14:57 +0530

    done

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx' 
 
+function getUserIdFromURL() {
+  const params = new URLSearchParams(window.location.search);
+  return params.get('user_id');
+}
+
+// Get the chatbotId from URL parameters or use a default value
+const user_id = getUserIdFromURL() || '1234';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App user_id="deea696a-593c-411a-aa06-d5b906e34402"/>
-  </React.StrictMode>,
-)
+    <App user_id={user_id} />
+  </React.StrictMode>
+);
